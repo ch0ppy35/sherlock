cbed96ec92de4858964d0d5a5c83ba0a293ec3a1
Mike Miller <chop3593@gmail.com>
2024-08-14 22:34:42 -0600

    Clean up commands some

--- cmd/run.go
+++ cmd/run.go
@@ -15,18 +15,17 @@ var configFile string
 
 // runCmd represents the run command
 var runCmd = &cobra.Command{
-	Use:   "run",
-	Short: "Run DNS tests based on the provided configuration",
+	Use:                   "run --config <path/to/config.yaml>",
+	DisableFlagsInUseLine: true,
+	Example:               "sherlock run --config path/to/config.yaml",
+	Short:                 "Run DNS tests based on the provided configuration",
 	Long: `Run DNS tests for the hosts specified in the configuration file. The command will
 execute all tests defined in the config and only report errors at the end. This ensures that
 all tests are run in a single execution without needing to rerun the tool.
 
 The configuration file should be in YAML format and specify the expected DNS records for
 the hosts to be tested. The tests will verify if the actual DNS records match the expected
-values and report any discrepancies after all tests are completed.
-
-Example usage:
-  sherlock run --config path/to/config.yaml`,
+values and report any discrepancies after all tests are completed.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		runTests()
 	},
@@ -50,6 +49,6 @@ func runTests() {
 func init() {
 	rootCmd.AddCommand(runCmd)
 
-	runCmd.PersistentFlags().StringVarP(&configFile, "config", "c", "", "config file (should be in config/config.yaml)")
+	runCmd.PersistentFlags().StringVarP(&configFile, "config", "c", "", "config file (config/config.yaml)")
 	runCmd.MarkPersistentFlagRequired("config")
 }
--- cmd/test.go
+++ cmd/test.go
@@ -12,8 +12,10 @@ import (
 
 // testCmd represents the test command
 var testCmd = &cobra.Command{
-	Use:   "test",
-	Short: "Run DNS tests based on provided parameters",
+	Use:                   "test --type <a|aaaa|cname|mx|txt|ns> --host <hostname> --expected <record1,record2,...> --server <dns-server>",
+	DisableFlagsInUseLine: true,
+	Example:               `sherlock test --type a --host example.com --expected "10.0.0.100" --server 1.1.1.1`,
+	Short:                 "Run DNS tests based on provided parameters",
 	Long: `The test command allows you to perform DNS queries for specific record types and 
 compare the results with the expected values.
 
@@ -23,9 +25,6 @@ DNS server, the type of record to query, the expected values, and the host you
 want to look up. It performs the query and then compares the actual results against 
 the expected values.
 
-Example usage:
-	sherlock test --type a --host example.com --expected "10.0.0.100" --server 1.1.1.1 
-
 Flags:
 	--type string   The type of DNS record to query (e.g., a, aaaa, cname, mx, txt, ns)
 	--host string   The hostname to look up (e.g., example.com)
@@ -34,12 +33,13 @@ Flags:
 	Run: func(cmd *cobra.Command, args []string) {
 		testType, expectedValues, dnsServer, host, err := parseFlags(cmd)
 		if err != nil {
-			fmt.Println(err)
-			return
+			ui.PrintMsgWithStatus("ERROR", "red", "Error: %v\n\n", err)
+			cmd.Usage()
+			os.Exit(1)
 		}
 
 		if err := runDNSQueryAndCompare(testType, expectedValues, dnsServer, host); err != nil {
-			ui.PrintMsgWithStatus("FAIL", "red", "Test failed, check above\n")
+			ui.PrintMsgWithStatus("FAIL", "red", "Test failed: %v\n", err)
 			os.Exit(1)
 		} else {
 			ui.PrintMsgWithStatus("GOOD", "green", "Record matches!\n")
@@ -54,7 +54,7 @@ func parseFlags(cmd *cobra.Command) (string, []string, string, string, error) {
 	host, _ := cmd.Flags().GetString("host")
 
 	if testType == "" || len(expectedValues) == 0 || dnsServer == "" || host == "" {
-		return "", nil, "", "", fmt.Errorf("provide a test type, host, expected values, and DNS server")
+		return "", nil, "", "", fmt.Errorf("all flags (--type, --host, --expected, --server) are required")
 	}
 
 	return testType, expectedValues, dnsServer, host, nil
@@ -80,6 +80,6 @@ func init() {
 
 	testCmd.Flags().StringP("type", "t", "", "DNS record type (e.g., a, aaaa, cname, mx, txt, ns)")
 	testCmd.Flags().StringSliceP("expected", "e", []string{}, "Expected DNS records, comma-separated")
-	testCmd.Flags().StringP("server", "s", "", "DNS server to query (1.1.1.1)")
-	testCmd.Flags().StringP("host", "H", "", "The host you want to look up (example.com)")
+	testCmd.Flags().StringP("server", "s", "", "DNS server to query (e.g., 1.1.1.1)")
+	testCmd.Flags().StringP("host", "H", "", "The host you want to look up (e.g., example.com)")
 }
